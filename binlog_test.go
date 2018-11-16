e740633152be9ed1e4b003494b5c2fa53acc27c8
arkady <larytet@yahoo.com>
2018-11-16 21:44:00 +0200

    Update

--- binlog_test.go
+++ binlog_test.go
@@ -1,10 +1,17 @@
 package binlog
 
 import (
-	"fmt"
-	"reflect"
-	"sync/atomic"
 	"testing"
-	"time"
 	"unsafe"
 )
+
+func BenchmarkCheckStringPointer(b *testing.B) {
+	s := "Hello, world"
+	p0 := uintptr(unsafe.Pointer(&s))
+	for i := 0; i < b.N; i++ {
+		p := uintptr(unsafe.Pointer(&s))
+		if p != p0 {
+			b.Fatalf("Different pointers %v %v", p0, p)
+		}
+	}
+}
