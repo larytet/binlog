e0ac8ada05b4560abb3bbdf968d12c25714995fc
arkady <larytet@yahoo.com>
2018-11-16 23:14:56 +0200

    Update

--- binlog.go
+++ binlog.go
@@ -11,6 +11,7 @@ type Binlog struct {
 
 // Read the executable, look for "hmst", assume that this is where all strings are
 // this is my base address for all strings in the system
+// See https://www.jonathan-petitcolas.com/2014/09/25/parsing-binary-files-in-go.html
 func Init() *Binlog {
 	executable, err := os.Executable()
 	if err != nil {
