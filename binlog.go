4d4894b7e6ff11e70aca55af48a3efe95cb5dcc9
arkady <larytet@yahoo.com>
2018-11-16 23:14:32 +0200

    Update

--- binlog.go
+++ binlog.go
@@ -5,7 +5,13 @@ import (
 	"os"
 )
 
-func Init() {
+type Binlog struct {
+	baseOffset uintptr
+}
+
+// Read the executable, look for "hmst", assume that this is where all strings are
+// this is my base address for all strings in the system
+func Init() *Binlog {
 	executable, err := os.Executable()
 	if err != nil {
 		log.Fatal("Failed to get eecutable", err)
@@ -15,7 +21,11 @@ func Init() {
 		log.Fatalf("Error while opening file %s %v", executable, err)
 	}
 	defer file.Close()
+	base := getOffset
 
+	var binlog Binlog
+
+	return &binlog
 }
 
 func PrintUint32(s string, args ...uint32) {
