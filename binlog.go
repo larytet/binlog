f97ce755b738463355a006c716cd960126557e41
arkady <larytet@yahoo.com>
2018-11-16 22:57:37 +0200

    Update

--- binlog.go
+++ binlog.go
@@ -2,6 +2,7 @@ package binlog
 
 import (
 	"log"
+	"path/filepath"
 	"reflect"
 	"unsafe"
 )
@@ -9,6 +10,13 @@ import (
 var baseString string = ""
 var baseStringAddress uintptr = (*reflect.StringHeader)(unsafe.Pointer(&baseString)).Data
 
+func Init() {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func PrintUint32(s string, args ...uint32) {
-	log.Printf("%v", unsafe.Pointer(&s))
+
 }
