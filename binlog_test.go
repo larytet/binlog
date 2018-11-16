41f18670680462a4e4cf4b618937186bdb5760c1
arkady <larytet@yahoo.com>
2018-11-16 22:22:32 +0200

    Update

--- binlog_test.go
+++ binlog_test.go
@@ -1,6 +1,7 @@
 package binlog
 
 import (
+	"reflect"
 	"testing"
 	"unsafe"
 )
@@ -45,5 +46,23 @@ func TestStringLocationGlobal(t *testing.T) {
 	p3 := p1 + align(uintptr(len(s1)))
 	if p1 != p2 && p0 != p3 {
 		t.Fatalf("Bad locations %x %x, expected %x %x", p0, p1, p2, p3)
+	}
+}
+
+func TestStringLocationGlobalLocal(t *testing.T) {
+	s1 := "Hello, world2"
+	p0 := uintptr(unsafe.Pointer(&s0))
+	p1 := uintptr(unsafe.Pointer(&s1))
+	if p0 != p1 {
+		//t.Fatalf("Bad locations %x %x", p0, p1)
+	}
+}
+
+func TestStringLocationGlobalLocalHeader(t *testing.T) {
+	s1 := "Hello, world1"
+	hdr0 := (*reflect.StringHeader)(unsafe.Pointer(&s0))
+	hdr1 := (*reflect.StringHeader)(unsafe.Pointer(&s1))
+	if hdr0.Data+0x100 > hdr1.Data {
+		t.Fatalf("Bad locations %x %x", hdr0.Data, hdr1.Data)
 	}
 }
