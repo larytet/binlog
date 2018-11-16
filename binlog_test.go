ee8a9ba5e5380f62089465cee566d58d59501f72
arkady <larytet@yahoo.com>
2018-11-16 22:12:14 +0200

    Update

--- binlog_test.go
+++ binlog_test.go
@@ -44,6 +44,6 @@ func TestStringLocationGlobal(t *testing.T) {
 	p2 := p0 + align(uintptr(len(s0)))
 	p3 := p1 + align(uintptr(len(s1)))
 	if p1 != p2 && p0 != p3 {
-		t.Fatalf("Bad locations %x %x, expected %x %x, %d, %d, %d", p0, p1, p2, p3, align(uintptr(len(s0))), len(s0), align(uintptr(len(s1))))
+		t.Fatalf("Bad locations %x %x, expected %x %x", p0, p1, p2, p3)
 	}
 }
