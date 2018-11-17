39db739576e364b72bfdd513db425b91ec1666d0
arkady <larytet@yahoo.com>
2018-11-17 15:08:40 +0200

    Update

--- binlog.go
+++ binlog.go
@@ -6,67 +6,25 @@ import (
 	"log"
 	//"runtime"
 	//	"os"
-	_ "unsafe"
+	"unsafe"
 )
 
 type Binlog struct {
 	baseOffset uintptr
 }
 
+// moduledata is a dead end?
 // See https://stackoverflow.com/questions/48445593/go-function-definition-in-another-package
 // https://golang.org/cmd/cgo/#hdr-Go_references_to_C
-
 //go:noescape
 //go:linkname runtime_firstmoduledata runtime.firstmoduledata
-var runtime_firstmoduledata uintptr
+//var Runtime_firstmoduledata uintptr
 
-// Straight from https://golang.org/src/runtime/symtab.go?m=text
-type functab struct {
-	entry   uintptr
-	funcoff uintptr
-}
-
-type textsect struct {
-	vaddr    uintptr // prelinked section vaddr
-	length   uintptr // section length
-	baseaddr uintptr // relocated section address
-}
-
-type moduledata struct {
-	pclntable             []byte
-	ftab                  []functab
-	filetab               []uint32
-	findfunctab           uintptr
-	minpc, maxpc          uintptr
-	text, etext           uintptr
-	noptrdata, enoptrdata uintptr
-	data, edata           uintptr
-	bss, ebss             uintptr
-	noptrbss, enoptrbss   uintptr
-	end, gcdata, gcbss    uintptr
-	types, etypes         uintptr
-	textsectmap           []textsect
-	typelinks             []int32 // offsets from types
-	itablinks             []*itab
-
-	ptab []ptabEntry
-
-	pluginpath string
-	pkghashes  []modulehash
-
-	modulename   string
-	modulehashes []modulehash
-
-	hasmain uint8 // 1 if module contains the main function, 0 otherwise
-
-	gcdatamask, gcbssmask bitvector
-
-	typemap map[typeOff]*_type // offset to *_rtype in previous module
-
-	bad bool // module failed to load and should be ignored
-
-	next *moduledata
-}
+// Straight from https://github.com/larytet/restartable/blob/master/restartable.go
+// and https://golang.org/src/runtime/symtab.go?m=text
+// See also https://stackoverflow.com/questions/37251108/golang-fetch-all-all-filepathes-from-compiled-file
+// https://blog.altoros.com/golang-internals-part-6-bootstrapping-and-memory-allocator-initialization.html
+// http://www.alangpierce.com/blog/2016/03/17/adventures-in-go-accessing-unexported-functions/
 
 func Init() *Binlog {
 	var firstmoduledata *moduledata = (*moduledata)(unsafe.Pointer(&runtime_firstmoduledata))
